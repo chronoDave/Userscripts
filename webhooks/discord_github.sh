fa087ec9bbddd2dabb280c6afd616ee3ee47ab86
chronoDave <davidwolters6758@gmail.com>
2020-09-27 14:02:34 +0200

    Added Discord.sh

---
+++ webhooks/discord_github.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+case $1 in
+  "success" )
+    EMBED_COLOR=3066993
+    STATUS="Passed"
+    AVATAR="https://i.imgur.com/IHAzV8k.jpg"
+    ;;
+    
+  "failure" )
+    EMBED_COLOR=15158332
+    STATUS="Failed"
+    AVATAR="https://i.imgur.com/niA0XYG.jpg"
+    ;;
+    
+  * )
+    EMBED_COLOR=0
+    STATUS="Unknown"
+    AVATAR="https://i.imgur.com/XoGketV.jpg"
+    ;;
+esac
+
+COMMIT_TITLE="$(git log -1 --pretty=%s)"
+COMMIT_DESCRIPTION="$(git log -1 --pretty=%b)"
+
+WEBHOOK_DATA='{
+  "embeds": [{
+	"author": {
+		"name": "'$STATUS' - '$GITHUB_WORKFLOW' #'$GITHUB_RUN_NUMBER' ('$GITHUB_ACTION')",
+		"url": "'$GITHUB_SERVER_URL'/'$GITHUB_REPOSITORY'/actions/runs/'$GITHUB_RUN_ID'",
+		"icon_url": "https://i.imgur.com/Z3lo7tA.png"
+	},
+	"title": "'$COMMIT_TITLE'",
+	"description": "'$COMMIT_DESCRIPTION'",
+	"thumbnail": {
+		"url": "'$AVATAR'"
+	},
+	"fields": [{
+		"name": "Author",
+		"value": "[`'$GITHUB_ACTOR'`]('$GITHUB_SERVER_URL'/'$GITHUB_ACTOR')",
+		"inline": true
+	}, {
+		"name": "Commit",
+		"value": "[`'${GITHUB_SHA:0:7}'`]('$GITHUB_SERVER_URL'/'$GITHUB_REPOSITORY'/commit/'$GITHUB_SHA')",
+		"inline": true
+	}],
+    "color": '$EMBED_COLOR',
+    "timestamp": "'$(date --utc +%FT%TZ)'"
+  }]
+}'
+
+curl --fail \
+	--progress-bar \
+	-A "GitHubActions-Webhook" \
+	-H Content-Type:application/json \
+	-H X-Author:Chronocide#5250 \
+	-d "${WEBHOOK_DATA}" "${WEBHOOK_URL}" \
