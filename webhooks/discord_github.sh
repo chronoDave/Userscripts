cb9d41f375e4a37adac25c0c7f5bff4ceb7c54b7
Chronocide <davidwolters6758@gmail.com>
2020-12-13 19:41:14 +0100

    Update discord_github.sh

--- webhooks/discord_github.sh
+++ webhooks/discord_github.sh
@@ -26,12 +26,15 @@ COMMIT_DESCRIPTION="$(git log -1 --pretty=%b)"
 WEBHOOK_DATA='{
   "embeds": [{
 	"author": {
-		"name": "'$STATUS' - '$GITHUB_WORKFLOW' #'$GITHUB_RUN_NUMBER' ('$GITHUB_ACTION')",
+		"name": "'$STATUS' - '$GITHUB_REPOSITORY'",
 		"url": "'$GITHUB_SERVER_URL'/'$GITHUB_REPOSITORY'/actions/runs/'$GITHUB_RUN_ID'",
 		"icon_url": "https://i.imgur.com/Z3lo7tA.png"
 	},
 	"title": "'$COMMIT_TITLE'",
 	"description": "'$COMMIT_DESCRIPTION'",
+  "footer": {
+    "text": "'$GITHUB_WORKFLOW' #'$GITHUB_RUN_NUMBER' ('$GITHUB_ACTION')"
+  },
 	"thumbnail": {
 		"url": "'$AVATAR'"
 	},
