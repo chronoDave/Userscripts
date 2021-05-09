c63712d6482b8a66acb6190e184e6f5506a05b54
chronoDave <davidwolters6758@gmail.com>
2021-05-09 18:47:27 +0200

    Swapped success flag

--- webhooks/github_discord.js
+++ webhooks/github_discord.js
@@ -1,7 +1,7 @@
 const { execFileSync } = require('child_process');
 const https = require('https');
 
-const [url, failed] = process.argv.slice(2);
+const [url, success] = process.argv.slice(2);
 
 const COMMIT_TITLE = execFileSync('git', ['log', '-1', '--pretty=%s']).toString();
 const COMMIT_DESCRIPTION = execFileSync('git', ['log', '-1', '--pretty=%b']).toString();
@@ -26,20 +26,20 @@ const data = {
       url: GITHUB_REPOSITORY_URL,
       icon_url: 'https://i.imgur.com/Z3lo7tA.png'
     },
-    title: `${failed ? 'Failed' : 'Passed'} - ${COMMIT_TITLE}`,
+    title: `${success ? 'Success' : 'Passed'} - ${COMMIT_TITLE}`,
     description: `${COMMIT_DESCRIPTION.slice(0, 2047)}`,
-    color: failed ?
-      '15158332' :
-      '3066993',
+    color: success ?
+      '3066993' :
+      '15158332',
     timestamp: new Date().toUTCString(),
     footer: {
       text: `${GITHUB_WORKFLOW} #${GITHUB_RUN_NUMBER}`,
       url: `${GITHUB_REPOSITORY_URL}/actions/runs/${GITHUB_RUN_ID}`
     },
     thumbnail: {
-      url: failed ?
-        'https://i.imgur.com/niA0XYG.jpg' :
-        'https://i.imgur.com/IHAzV8k.jpg'
+      url: success ?
+        'https://i.imgur.com/IHAzV8k.jpg' :
+        'https://i.imgur.com/niA0XYG.jpg'
     },
     fields: [{
       name: 'Author',
